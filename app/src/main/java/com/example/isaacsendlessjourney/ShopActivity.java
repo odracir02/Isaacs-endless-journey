d471e802d4584e5b72d89bfa621c41a8005609c8
Odracir02 <ricardosastrelopez@gmail.com>
2023-02-08 23:01:30 +0100

    Added Activity

--- app/src/main/java/com/example/isaacsendlessjourney/ShopActivity.java
+++ app/src/main/java/com/example/isaacsendlessjourney/ShopActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class StartView extends AppCompatActivity {
+public class ShopActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_start_view);
+        setContentView(R.layout.activity_shop);
     }
 }
